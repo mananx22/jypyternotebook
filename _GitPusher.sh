f8c473313d413f6c775898c37f23c6056dec0303
mananx22 <motorollaman27@gmail.com>
2025-02-23 12:02:06 -0800

    tweaks

--- _GitPusher.sh
+++ _GitPusher.sh
@@ -6,6 +6,6 @@
 Branch=$(git status | grep On | awk '{print $3}')
 git add .
 git commit -m "$*"
-echo "committing with message \"$*\" "
+echo "--------------- committing with message \"$*\" "
 git push -u origin $Branch
-echo "Pushed to ===> $Branch"
+echo "---------------  Pushed to ===> $Branch"
