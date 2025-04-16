f18d2fff3dbb26df4af9f0a0f407e7fdf57e9452
mananx22 <motorollaman27@gmail.com>
2025-04-15 22:45:36 -0700

    added w2vec

---
+++ _run_notebook.sh
@@ -0,0 +1,11 @@
+# git add .
+# git commit -m "$*"
+# echo "committing with message \"$*\" "
+# git push
+jupyter notebook
+# Branch=$(git status | grep On | awk '{print $3}')
+# git add .
+# git commit -m "$*"
+# echo "--------------- committing with message \"$*\" "
+# git push -u origin $Branch
+# echo "---------------  Pushed to ===> $Branch"
