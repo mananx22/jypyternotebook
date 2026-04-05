e84f21866a87495e1715e9c09ca8707be250a3bd
mananx22 <motorollaman27@gmail.com>
2026-04-05 06:24:11 -0700

    first flask form

--- Mlops_project/Flask_basics/flask/app.py
+++ Mlops_project/Flask_basics/flask/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "welcome to first flask app, Hi"
+    return render_template("index.html")
+
+@app.route("/form", methods=['GET','POST'] )
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f" Welcome {name}, how are you"
+    else:
+        return render_template("form.html")
 
 if __name__=="__main__":
     app.run(debug=True)
