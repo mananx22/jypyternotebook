bc0075870e61bd572a57019140ddd136b9886430
mananx22 <motorollaman27@gmail.com>
2026-04-05 07:06:48 -0700

    applying loop in jinja template

--- Mlops_project/Flask_basics/flask/app.py
+++ Mlops_project/Flask_basics/flask/app.py
@@ -26,6 +26,17 @@ def success(score):
 
     return render_template('results.html', results=res, scr=score)
 
+# 2) variable rule with loop 
+@app.route("/successloop/<int:score>")
+def successloop(score):
+    res = ""
+    if score > 50:
+        res = "Passed"
+    else:
+        res = "failed"
+    exp = {"res":res,"score":score}
+    return render_template("./loop_result.html", expression=exp)
+
 
 if __name__=="__main__":
     app.run(debug=True)
